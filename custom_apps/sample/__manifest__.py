efb9c7ce61ace66c04f45406232efadc7503efd9
BitongJam <jamesortizalter@gmail.com>
2024-07-14 11:11:38 +0800

    first commit

---
+++ custom_apps/sample/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Sample',
+    'version': '1.0',
+    'description': 'sample module',
+    'summary': 'sample module',
+    'license': 'LGPL-3',
+    'auto_install': False,
+    'application': False,
+}
