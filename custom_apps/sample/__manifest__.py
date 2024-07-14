803393f4e682be6f4a9eb565b19a8cc41d30366a
BitongJam <jamesortizalter@gmail.com>
2024-07-14 11:13:01 +0800

    [REM] sample folder

--- custom_apps/sample/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    'name': 'Sample',
-    'version': '1.0',
-    'description': 'sample module',
-    'summary': 'sample module',
-    'license': 'LGPL-3',
-    'auto_install': False,
-    'application': False,
-}
